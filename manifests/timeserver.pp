02c4e5b98b2abe3ccc1d5a60d9ed552592b1a155
lorello <lorenzo.salvadorini@softecspa.it>
2016-02-29 15:44:42 +0100

    documented define ntp::timeserver

--- manifests/timeserver.pp
+++ manifests/timeserver.pp
@@ -1,3 +1,10 @@
+# == Define: ntp::timeserver
+#
+# Use of the class through a define
+# to make possibile to override parameters in 
+# Puppet 2.7, not more needed in Puppet >= 3
+# and to be removed.
+#
 define ntp::timeserver (
   $timeserver1 = 'ie.pool.ntp.org',
   $timeserver2 = '0.europe.pool.ntp.org',
